45eb1630e21c38f0acd708b2dcd26db3a79fca2f
Teacher <sapdevm@gmail.com>
2025-05-05 21:18:19 +0500

    1.7.0

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,33 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::middleware('guest')
+    ->group(function () {
+        Route::get('register', [RegisterController::class, 'create'])->name('register');
+        Route::post('register', [RegisterController::class, 'store']);
+    });
+
+Route::middleware('guest')
+    ->group(function () {
+        Route::get('login', [LoginController::class, 'create'])->name('login');
+        Route::post('login', [LoginController::class, 'store']);
+    });
+
+Route::middleware('auth')
+    ->group(function () {
+        Route::post('logout', [LoginController::class, 'destroy'])->name('logout');
+    });
+
+Route::controller(HomeController::class)
+    ->group(function () {
+        Route::get('', 'index')->name('home');
+    });
+
+Route::middleware('auth')
+    ->group(function () {
+
+    });
